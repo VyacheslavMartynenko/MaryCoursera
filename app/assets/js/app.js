fe0c085f57f4ecf36c3e0acf11e0ef2c0b2aff24
vyacheslavmartynenko <evilacer5755g@gmail.com>
2016-12-03 00:55:50 +0200

    add zet and wanga pages

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -5,6 +5,12 @@ app.config(function ($routeProvider) {
         .when("/main", {
             templateUrl: "components/main.html"
         })
+        .when("/wanga", {
+            templateUrl: "components/wanga.html"
+        })
+        .when("/zet", {
+            templateUrl: "components/zet.html"
+        })
         .otherwise({
             templateUrl: "components/main.html"
         });
