64783699c8ede8b91ebd3e50ce4ace5213bc8ee5
vyacheslavmartynenko <evilacer5755g@gmail.com>
2016-12-04 01:15:54 +0200

    micro fix

--- app/assets/js/books.js
+++ app/assets/js/books.js
@@ -1,2 +1,2 @@
-$("#books").animate({"margin-right":"20px"}, { duration: 1500, queue: false });
-$("#books").animate({"opacity": "1"}, { duration: 2500, queue: false });
+$("#books").animate({"margin-right":"20px"}, { duration: 2000, queue: false });
+$("#books").animate({"opacity": "1"}, { duration: 3000, queue: false });
